7741a7e58bd7ac5a5d1f60f0e4372952c7bb6618
Alecsandr86 <alecsandrcherneckiy@gmail.com>
2016-03-16 16:17:44 +0200

    commit-2

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,12 @@
 //= ../../bower_components/jquery/dist/jquery.min.js
 //= ../../bower_components/velocity/velocity.min.js
 //= ../../bower_components/jquery.easing/js/jquery.easing.min.js
+//= ../../bower_components/mixitup/build/jquery.mixitup.min.js
+
+//= ../../bower_components/fullpage.js/vendors/jquery.easings.min.js
+//= ../../bower_components/fullpage.js/vendors/jquery.slimscroll.min.js
+//= ../../bower_components/fullpage.js/jquery.fullPage.min.js
+//= ../../bower_components/waypoints/lib/jquery.waypoints.min.js
 
 
 /*
--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -4,6 +4,21 @@ window.log = function(param){
 };
 $(document).ready(function(){
 
+    /*******************************
+     *   ПЛАВНЫЙ ПЕРЕХОД ПО ЯКОРЯМ
+     *******************************/
+    $('a[href^="#s"]').click(function(){
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top},2000,"easeInOutExpo");
+        return false;
+    });
+    /*******************************
+     *   \ПЛАВНЫЙ ПЕРЕХОД ПО ЯКОРЯМ\
+     *******************************/
+
+
+
     //Chrome Smooth Scroll
     try {
         $.browserSelector();
@@ -14,15 +29,112 @@ $(document).ready(function(){
 
     };
 
-    $('.language li').bind('click', function(){
-        $('.language li').removeClass('active');
-        $(this).addClass("active");
-    });
+    //$('.language li').bind('mouseenter mouseleave', function(){
+    //    $('.language li').removeClass('active');
+    //    $(this).addClass("active");
+    //});
 
     $('.commandes .item').bind('mouseenter mouseleave', function(){
-        $('.commandes .item').removeClass('active');
+        //$('.commandes .item').removeClass('active');
+        $(this).toggleClass('active');
+    });
+    $('.list-mixitup li').bind('click',function(){
+        $('.list-mixitup li').removeClass('active');
         $(this).addClass('active');
+    });
+    $('.services-catalog .item').bind('mouseenter mouseleave', function(){
+        $(this).toggleClass('active');
+    })
+
+
+
+
+    $('.sections').find('footer').fadeOut();
+    $('.wr-nav').find('.nav').fadeOut();
+
+    $('.wrapper-fluid').fullpage({
+        //responsiveHeight : 945,
+        //slidesNavigation: false,
+        anchors: ['page1', 'page2', 'page3', 'page4','page5'],
+        scrollOverflow: true,
+        //autoScrolling: false,
+        resize : true,
+        sectionSelector: '.sections',
+        afterLoad: function(){
+            $('.active').find('footer').fadeIn({
+                duration: 500,
+                easing: 'easeInQuad'
+            });
+            $('.active .wr-nav').find('.nav').fadeIn({
+                duration: 500,
+                easing: 'easeInQuad'
+            });
+
+        },
+        onLeave: function(){
+            $('.sections').find('footer').fadeOut();
+            $('.wr-nav').find('.nav').fadeOut();
+        }
+
     });
 
 
+    $(document).on('click', '.moveDown', function(){
+        $.fn.fullpage.moveSectionDown();
+    });
+    $(document).on('click', '.page1', function(){
+        $.fn.fullpage.moveTo('page1');
+    });
+    $(document).on('click', '.page2', function(){
+        $.fn.fullpage.moveTo('page2');
+    });
+    $(document).on('click', '.page3', function(){
+        $.fn.fullpage.moveTo('page3');
+    });
+    $(document).on('click', '.page4', function(){
+        $.fn.fullpage.moveTo('page4');
+    });
+    $(document).on('click', '.page5', function(){
+        $.fn.fullpage.moveTo('page5');
+    });
+
+
+
+
+    $('.services-catalog').mixItUp({
+        load: {
+            filter: '.category-1'
+        }
+    });
+
+
+
+
+
+    /*******************************
+     *       КНОПКА НА ВЕРХ
+     *******************************/
+
+    // кнопка
+    //var UpButton = $('.UP-button');
+    //
+    //UpButton.css('opacity', 0);
+    //
+    //$('#header').waypoint(function(direction){
+    //    if (direction === 'down') {
+    //        alert('ok');
+    //        UpButton.removeClass('animated fadeOutDown').css('opacity', 0);
+    //        UpButton.css('opacity', 1).addClass('animated fadeInUp');
+    //    }
+    //    else {
+    //        UpButton.removeClass('animated fadeInUp');
+    //        UpButton.addClass('animated fadeOutDown').css('opacity', 0);
+    //    }
+    //}, {
+    //    offset: '-15%'
+    //});
+
+    /*******************************
+     *       КНОПКА НА ВЕРХ
+     *******************************/
 });
