701a839385cac3652cf499f71bceba3caccbe934
Alecsandr86 <alecsandrcherneckiy@gmail.com>
2016-03-17 21:40:14 +0200

    commit-4

--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -4,30 +4,15 @@ window.log = function(param){
 };
 $(document).ready(function(){
 
-    /*******************************
-     *   ПЛАВНЫЙ ПЕРЕХОД ПО ЯКОРЯМ
-     *******************************/
-    $('a[href^="#s"]').click(function(){
-        var target = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(target).offset().top},2000,"easeInOutExpo");
-        return false;
-    });
-    /*******************************
-     *   \ПЛАВНЫЙ ПЕРЕХОД ПО ЯКОРЯМ\
-     *******************************/
-
-
-
     //Chrome Smooth Scroll
-    try {
-        $.browserSelector();
-        if($("html").hasClass("chrome")) {
-            $.smoothScroll();
-        }
-    } catch(err) {
-
-    };
+    //try {
+    //    $.browserSelector();
+    //    if($("html").hasClass("chrome")) {
+    //        $.smoothScroll();
+    //    }
+    //} catch(err) {
+    //
+    //};
 
     //$('.language li').bind('mouseenter mouseleave', function(){
     //    $('.language li').removeClass('active');
@@ -49,65 +34,88 @@ $(document).ready(function(){
 
 
 
-    $('.sections').find('footer').fadeOut();
-    $('.sections').find('.up-button').fadeOut();
-    $('.wr-nav').find('.nav').fadeOut();
-    $('.sections').find('.form-style').removeClass('fadeInLeft animated');
-    $('.sections').find('.form-style').animate({'opacity':'0'});
 
-    $('.wrapper-fluid').fullpage({
-        //responsiveHeight : 945,
-        //slidesNavigation: false,
-        anchors: ['page1', 'page2', 'page3', 'page4','page5'],
-        scrollOverflow: true,
-        //autoScrolling: false,
-        resize : true,
-        sectionSelector: '.sections',
-        afterLoad: function(){
-            $('.active').find('footer').fadeIn({
-                duration: 500,
-                easing: 'easeInQuad'
-            });
-            $('.active .wr-nav').find('.nav').fadeIn({
-                duration: 500,
-                easing: 'easeInQuad'
-            });
-            $('.active').find('.up-button').fadeIn({
-                duration: 500,
-                easing: 'easeInQuad'
-            });
-            $('.active').find('.form-style').animate({'opacity':'1'}).addClass('fadeInLeft animated');
 
-        },
-        onLeave: function(){
+    $(window).bind("load resize",function() {
+
+        var height = $(window).height(); //естественно название переменной можно поставить любое )))
+        var width = $(window).width();
+
+        if(width <= 1029){
+            $.fn.fullpage.destroy('all');
+        }else{
+
             $('.sections').find('footer').fadeOut();
-            $('.wr-nav').find('.nav').fadeOut();
             $('.sections').find('.up-button').fadeOut();
+            $('.wr-nav').find('.nav').fadeOut();
             $('.sections').find('.form-style').removeClass('fadeInLeft animated');
             $('.sections').find('.form-style').animate({'opacity':'0'});
+
+            $('.wrapper-fluid').fullpage({
+                //responsiveHeight : 945,
+                //slidesNavigation: false,
+                anchors: ['page1', 'page2', 'page3', 'page4','page5'],
+                scrollOverflow: true,
+                //autoScrolling: false,
+                //resize : true,
+                sectionSelector: '.sections',
+                afterLoad: function(){
+                    $('.active').find('footer').fadeIn({
+                        duration: 500,
+                        easing: 'easeInQuad'
+                    });
+                    $('.active .wr-nav').find('.nav').fadeIn({
+                        duration: 500,
+                        easing: 'easeInQuad'
+                    });
+                    $('.active').find('.up-button').fadeIn({
+                        duration: 500,
+                        easing: 'easeInQuad'
+                    });
+                    $('.active').find('.form-style').animate({'opacity':'1'}).addClass('fadeInLeft animated');
+
+                },
+                onLeave: function(){
+                    $('.sections').find('footer').fadeOut();
+                    $('.wr-nav').find('.nav').fadeOut();
+                    $('.sections').find('.up-button').fadeOut();
+                    $('.sections').find('.form-style').removeClass('fadeInLeft animated');
+                    $('.sections').find('.form-style').animate({'opacity':'0'});
+                }
+
+            });
+
+
+            $(document).on('click', '.moveDown', function(){
+                $.fn.fullpage.moveSectionDown();
+            });
+            $(document).on('click', '.page1', function(){
+                $.fn.fullpage.moveTo('page1');
+            });
+            $(document).on('click', '.page2', function(){
+                $.fn.fullpage.moveTo('page2');
+            });
+            $(document).on('click', '.page3', function(){
+                $.fn.fullpage.moveTo('page3');
+            });
+            $(document).on('click', '.page4', function(){
+                $.fn.fullpage.moveTo('page4');
+            });
+            $(document).on('click', '.page5', function(){
+                $.fn.fullpage.moveTo('page5');
+            });
+
+
         }
 
     });
 
 
-    $(document).on('click', '.moveDown', function(){
-        $.fn.fullpage.moveSectionDown();
-    });
-    $(document).on('click', '.page1', function(){
-        $.fn.fullpage.moveTo('page1');
-    });
-    $(document).on('click', '.page2', function(){
-        $.fn.fullpage.moveTo('page2');
-    });
-    $(document).on('click', '.page3', function(){
-        $.fn.fullpage.moveTo('page3');
-    });
-    $(document).on('click', '.page4', function(){
-        $.fn.fullpage.moveTo('page4');
-    });
-    $(document).on('click', '.page5', function(){
-        $.fn.fullpage.moveTo('page5');
-    });
+
+
+
+
+
 
 
 
@@ -122,30 +130,7 @@ $(document).ready(function(){
 
 
 
-    /*******************************
-     *       КНОПКА НА ВЕРХ
-     *******************************/
 
-    // кнопка
-    //var UpButton = $('.UP-button');
-    //
-    //UpButton.css('opacity', 0);
-    //
-    //$('#header').waypoint(function(direction){
-    //    if (direction === 'down') {
-    //        alert('ok');
-    //        UpButton.removeClass('animated fadeOutDown').css('opacity', 0);
-    //        UpButton.css('opacity', 1).addClass('animated fadeInUp');
-    //    }
-    //    else {
-    //        UpButton.removeClass('animated fadeInUp');
-    //        UpButton.addClass('animated fadeOutDown').css('opacity', 0);
-    //    }
-    //}, {
-    //    offset: '-15%'
-    //});
 
-    /*******************************
-     *       КНОПКА НА ВЕРХ
-     *******************************/
+
 });
